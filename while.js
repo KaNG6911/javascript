f36d5b1735628cf1f600fd7dc81f0be46f02c68b
25LP5951 25LP5951 <gna.k6911@gmail.com>
2025-11-03 12:50:08 +0800

    oorclsonbd

---
+++ while.js
@@ -0,0 +1,25 @@
+// 1. Тооны цифрийн нийлбэрийг олох. (let n = 1234)
+// let num = 1237;
+// sum = 0;
+// while (num > 0) {
+//   sum += num % 10;
+//   num = Math.floor(num / 10);
+// }
+// console.log(sum);
+
+// hun = num.toString();
+// for (i = 0; i < hun.length; i++) {
+//   sum += Number(hun[i]);
+// }
+// console.log(sum);
+
+// 2. Тоог урвуулан бичих (let num = 12345 -> 54321)
+// let num = 87875 ;
+// sum = "";
+// hun = num.toString();
+// i = hun.length - 1;
+// while (i >= 0) {
+//   sum += hun[i];
+//   i--;
+// }
+// console.log(sum);
