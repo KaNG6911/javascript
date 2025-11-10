148fa9bcbe914c63ef4f244abae570cc5a8496e8
KaNG6911 <gna.k6911@gmail.com>
2025-11-10 08:51:52 +0800

    duuskal bn

--- nestedloop.js
+++ nestedloop.js
@@ -118,14 +118,69 @@
 // 1-ээс 10 хүртэлх үржвэрийн хүснэгт хэвлэ.
 // for (let i = 1; i <= 10; i++) {
 //   for (let j = 1; j <= 10; j++) {
-//     hun = i + "*" + j + "=" + i * j;
-//     console.log(hun);
+//     hun = i + '*' + j + '=' + i * j
+//     console.log(hun)
 //   }
 // }
 
 // Өгөгдсөн string доторх давтагдсан үсгүүдийг ол. Жишээ: "programming" → ['r','g','m']
+// const str = "programming";
+// let hun = []
+// for (let i = 0; i < str.length; i++) {
+//   for (let j = i + 1; j < str.length; j++) {
+//     if (str[i] === str[j] && !hun.includes(str[j])) {
+//       hun.push(str[j])
+//       break
+//     }
+//   }
+// }
+// console.log(hun)
+
 // Set ашиглалгүйгээр массив доторх давхардсан элементүүдийг арилгах. Жишээ: [1,2,2,3,4,4,5] → [1,2,3,4,5]
+// const arr = [1, 2, 2, 3, 4, 4, 5]
+// const hun = []
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr.length; j++) {
+//     if (arr[i] !== arr[j] && !hun.includes(arr[i]) && i !== j) {
+//       hun.push(arr[i])
+//     }
+//   }
+// }
+// console.log(hun)
+
 // Хоёр ангийн оюутнуудын дүнг харьцуулж, аль нь өндөр оноотойг илэрхийл. Жишээ: classA=[80,90,70], classB=[85,85,75] → [B,A,B]
+// const classA = [80, 90, 70, 85, 75]
+// const classB = [85, 85, 85, 90, 70]
+// let hun = []
+// for (let i = 0; i < classA.length; i++) {
+//   for (let j = 0; j < classB.length; j++) {
+//     if (i !== j) {
+//       continue
+//     }
+//     if (classA[i] > classB[j]) {
+//       hun.push('A')
+//       break
+//     } else {
+//       hun.push('B')
+//       break
+//     }
+//   }
+// }
+// console.log(hun)
+
 // Хоёр string-ийг double loop ашиглан substring эсэхийг шалга (includes ашиглахгүй). Жишээ: "hello world", "world" → true
+let hun = 'hello world',
+  sum = 'world'
+let bool = false
+for (let i = 0; i < hun.length; i++) {
+  for (let j = 0; j < sum.length; j++) {
+    if (hun[i] === sum[j]) {
+      bool = true
+      break
+    }
+  }
+}
+console.log(bool)
+
 // Хоёр массивын бүх элементүүдийн нийлбэрийг 2D массив хэлбэрээр гарга. Жишээ: [1,2] ба [3,4] → [[4,5],[5,6]]
 // Хоёр string-ийн адилхан үсэг хэдэн ширхэг байгаа болохыг тооц. Жишээ: "apple", "peach" → 3 (p, e, a)
